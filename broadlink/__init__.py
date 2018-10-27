dcc3671f4c89f86efc88d44f96cf8feeb49ad93c
keniji <42759206+keniji@users.noreply.github.com>
2018-10-27 23:03:13 +0800

    Update __init__.py
    Add support for WS2 (Broadlink / Honyar 86 type Wall Plug)

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -50,6 +50,7 @@ def gendevice(devtype, host, mac):
           hysen: [0x4EAD],  # Hysen controller
           S1C: [0x2722],  # S1 (SmartOne Alarm Kit)
           dooya: [0x4E4D]  # Dooya DT360E (DOOYA_CURTAIN_V2)
+          ws2: [0x4F19], # WS2 (Broadlink / Honyar 86 type Wall Plug)
           }
 
   # Look for the class associated to devtype in devices
@@ -843,6 +844,65 @@ class dooya(device):
         time.sleep(0.2)
         current = self.get_percentage()
     self.stop()
+
+
+class ws2(device):
+  def __init__ (self, host, mac, devtype):
+    device.__init__(self, host, mac, devtype)
+    self.type = "WS2"
+
+  def set_power(self, state):
+    """Sets the power state of the wall plug."""
+    packet = bytearray(16)
+    packet[0x00] = 0x0d
+    packet[0x02] = 0xa5
+    packet[0x03] = 0xa5
+    packet[0x04] = 0x5a
+    packet[0x05] = 0x5a
+    packet[0x06] = 0xb2 + (0x02 if state else 0x01)
+    packet[0x07] = 0xc0
+    packet[0x08] = 0x02
+    packet[0x0a] = 0x03
+    packet[0x0d] = 0x01
+    packet[0x0e] = 0x01 if state else 0
+
+    response = self.send_packet(0x6a, packet)
+
+    err = response[0x22] | (response[0x23] << 8)
+
+  def check_power(self):
+    """Returns the power state of the wall plug."""
+    packet = bytearray(16)
+    packet[0x00] = 0x0a
+    packet[0x02] = 0xa5
+    packet[0x03] = 0xa5
+    packet[0x04] = 0x5a
+    packet[0x05] = 0x5a
+    packet[0x06] = 0xae
+    packet[0x07] = 0xc0
+    packet[0x08] = 0x01
+
+    response = self.send_packet(0x6a, packet)
+    err = response[0x22] | (response[0x23] << 8)
+    if err == 0:
+        payload = self.decrypt(bytes(response[0x38:]))
+        if type(payload[0x4]) == int:
+          state = payload[0x0e]
+        else:
+          state = ord(payload[0x0e])
+        return state
+
+  def get_energy(self):
+    packet = bytearray([8, 0, 254, 1, 5, 1, 0, 0, 0, 45])
+    response = self.send_packet(0x6a, packet)
+    err = response[0x22] | (response[0x23] << 8)
+    if err == 0:
+      payload = self.decrypt(bytes(response[0x38:]))
+      if type(payload[0x07]) == int:
+        energy = int(hex(payload[0x07] * 256 + payload[0x06])[2:]) + int(hex(payload[0x05])[2:])/100.0
+      else:
+        energy = int(hex(ord(payload[0x07]) * 256 + ord(payload[0x06]))[2:]) + int(hex(ord(payload[0x05]))[2:])/100.0
+      return energy
 
 
 # Setup a new Broadlink device via AP Mode. Review the README to see how to enter AP Mode.
